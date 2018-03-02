8fc48d283ccf21fe6ddfa9dd503e14a48395191c
xiaobin <bill_xiaobin@163.com>
2018-03-03 00:00:18 +0800

    check http 502 to reload php-fpm

---
+++ auto_check.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+#desciption : auto check http 502 to restart php-fpm
+
+num=`cat /mnt/logs/nginx/access.log | grep -oP 'HTTP/1.1" 502' | cut -d ' ' -f | uniq -c | awk '{print $1}'`
+echo 'err_num:'$num
+
+pid=`ps -ef | grep 'master process (/mnt/server/php7020/etc/php-fpm.conf)' | grep -v grep | head -1 | awk '{print $2}'`
+echo 'pid:'$pid
+
+if [[ $num -gt 20 ]];then
+    kill $pid
+    /mnt/server/php7020/sbin/php-fpm -c /mnt/sever/php7020/etc/php.ini
+    chown -R www-data:www-data /var/run/php-fpm/
+    $num >> /mnt/logs/nginx/kill.log
+fi
