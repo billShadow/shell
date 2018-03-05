15311585d069cac3aa92ad858b1f33708ded5aee
xiaobin <bill_xiaobin@163.com>
2018-03-05 23:01:40 +0800

    nginx server command

--- auto_check.sh
+++ auto_check.sh
@@ -3,6 +3,9 @@
 #desciption : auto check http 502 to restart php-fpm
 
 num=`cat /mnt/logs/nginx/access.log | grep -oP 'HTTP/1.1" 502' | cut -d ' ' -f | uniq -c | awk '{print $1}'`
+# 或者这样,只是cut换成了awk命令
+#num=`cat /mnt/logs/nginx/access.log | grep -oP 'HTTP/1.1" 502' | awk '{print $2}' | uniq -c | awk '{print $1}'`
+
 echo 'err_num:'$num
 
 pid=`ps -ef | grep 'master process (/mnt/server/php7020/etc/php-fpm.conf)' | grep -v grep | head -1 | awk '{print $2}'`
---
+++ nginx_server.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+#author : bill_xiaobin@163.com
+#description : nginx server commnd
+
+echo "可执行动作start,stop,restart"
+read -p "请输入您需要执行的动作：" name
+
+case $name in
+    start)
+        /data/server/nginx/sbin/nginx -c /data/server/nginx/etc/nginx.conf
+        ;;
+    stop)
+        nginx_pid=`ps -ef | grep -v grep | grep "nginx: master process " | awk '{print $2}'`
+        kill $nginx_pid
+        ;;
+    restart)
+        /data/server/nginx/sbin/nginx -s reload
+        ;;
+    *)
+        echo 'not found option'
+        ;;
+esac
+exit 0
